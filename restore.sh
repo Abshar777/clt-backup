33f4c6acff9b7c8f1a15552d37fcfc9eade61b9b
Abshar777 <absharameen625@gmail.com>
2025-11-18 15:32:29 +0530

    doine

--- restore.sh
+++ restore.sh
@@ -9,26 +9,26 @@ AUTH_DB="admin"
 
 mongoimport --host "$HOST" --port "$PORT" \
   --username "$USER" --password "$PASS" --authenticationDatabase "$AUTH_DB" \
-  --db "$DB_NAME" --collection Booking --drop --file Booking.json --jsonArray
+  --db "$DB_NAME" --collection bookings --drop --file Booking.json --jsonArray
 
 mongoimport --host "$HOST" --port "$PORT" \
   --username "$USER" --password "$PASS" --authenticationDatabase "$AUTH_DB" \
-  --db "$DB_NAME" --collection Student --drop --file Student.json --jsonArray
+  --db "$DB_NAME" --collection students --drop --file Student.json --jsonArray
 
 mongoimport --host "$HOST" --port "$PORT" \
   --username "$USER" --password "$PASS" --authenticationDatabase "$AUTH_DB" \
-  --db "$DB_NAME" --collection User --drop --file User.json --jsonArray
+  --db "$DB_NAME" --collection users --drop --file User.json --jsonArray
 
 mongoimport --host "$HOST" --port "$PORT" \
   --username "$USER" --password "$PASS" --authenticationDatabase "$AUTH_DB" \
-  --db "$DB_NAME" --collection MainCourse --drop --file MainCourse.json --jsonArray
+  --db "$DB_NAME" --collection maincourses --drop --file MainCourse.json --jsonArray
 
 mongoimport --host "$HOST" --port "$PORT" \
   --username "$USER" --password "$PASS" --authenticationDatabase "$AUTH_DB" \
-  --db "$DB_NAME" --collection Notification --drop --file Notification.json --jsonArray
+  --db "$DB_NAME" --collection notifications --drop --file Notification.json --jsonArray
 
 mongoimport --host "$HOST" --port "$PORT" \
   --username "$USER" --password "$PASS" --authenticationDatabase "$AUTH_DB" \
-  --db "$DB_NAME" --collection Course --drop --file Course.json --jsonArray
+  --db "$DB_NAME" --collection courses --drop --file Course.json --jsonArray
 
 echo "Database restored successfully!"
